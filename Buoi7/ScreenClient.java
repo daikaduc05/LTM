e35789185991ad52d373d213736e20bc2c466911
daikaduc05 <danghoaiduc9@gmail.com>
2025-09-27 21:37:10 +0700

    init

---
+++ Buoi7/ScreenClient.java
@@ -0,0 +1,60 @@
+package Buoi7;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class ScreenClient extends JFrame {
+	Socket soc;
+	public static void main(String[] args) {
+		new ScreenClient();
+	}
+	int off = 50;
+	public ScreenClient() {
+		this.setTitle("Share Screen");
+		this.setSize(500, 400);
+		this.setDefaultCloseOperation(3);
+		try {
+			soc = new Socket("localhost",2345);
+		} catch(Exception e) {
+			System.exit(1);
+		}
+
+		this.setVisible(true);
+	}
+
+	public void paint(Graphics g) {
+		try {
+			DataInputStream bis = new DataInputStream(soc.getInputStream());
+			int n = bis.readInt();
+			byte tmp[] = bis.readNBytes(n);
+			
+			
+			ByteArrayInputStream bis1 = new ByteArrayInputStream(tmp);
+			BufferedImage img1 = ImageIO.read(bis1);
+			int w = this.getWidth()-2*off;
+			int h = this.getHeight()-2*off;
+			Image img2 = img1.getScaledInstance(w,h, Image.SCALE_SMOOTH);
+			
+			g.drawImage(img2, off, off, this.getWidth()-off, this.getHeight()-off, 
+					0, 0, w,h, null);
+			
+			this.repaint();
+		} catch (Exception e) {
+		}
+	}
+
+}
+
---
+++ Buoi7/ScreenServer.java
@@ -0,0 +1,98 @@
+package Buoi7;
+
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.imageio.ImageIO;
+
+public class ScreenServer {
+    public static void main(String[] args) {
+        new ScreenServer();
+    }
+
+    public ScreenServer() {
+        Screen s = new Screen();
+        // s.setPriority(10);
+        s.start();
+        try {
+            ServerSocket server = new ServerSocket(2345);
+            while (true) {
+                Socket soc = server.accept();
+                ScreenProcessing sp = new ScreenProcessing(soc);
+                // sp.setPriority(1);
+                sp.start();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+}
+
+class Screen extends Thread {
+    static byte[] tmp;
+    static int count = 0;
+
+    public void run() {
+        Robot r = null;
+        Rectangle capture = null;
+        try {
+            r = new Robot();
+            capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        } catch (Exception e) {
+
+        }
+        while (true) {
+            try {
+                BufferedImage img = r.createScreenCapture(capture);
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                ImageIO.write(img, "png", bos);
+                bos.flush();
+                tmp = bos.toByteArray();
+                count++;
+                if (count % 100 == 0)
+                    System.out.println(count + ":" + tmp.length);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
+
+class ScreenProcessing extends Thread {
+    Socket soc;
+
+    public ScreenProcessing(Socket soc) {
+        this.soc = soc;
+    }
+
+    int countNow;
+
+    public void run() {
+        while (true) {
+            try {
+                if (countNow == Screen.count) {
+                    Thread.sleep(1);
+                    continue;
+                }
+                byte[] tmp = Screen.tmp.clone();
+                countNow = Screen.count;
+                // System.out.println("count:"+Screen.count);
+                DataOutputStream output = new DataOutputStream(soc.getOutputStream());
+                output.writeInt(tmp.length);
+                output.write(tmp);
+                output.flush();
+                Thread.sleep(1);
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
